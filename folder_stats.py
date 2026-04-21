3cea15fb9ee86a90192bd52cb13cc9ed43e0b0d4
Pralhad Nasane <pralhad.work@gmail.com>
2026-04-21 12:01:58 +0530

    initial commit

---
+++ folder_stats.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+
+def calculate_stats(folder_path):
+    total_files = 0
+
+    for root, dirs, files in os.walk(folder_path):
+        total_files += len(files)
+
+    return total_files
+
+
+if __name__ == "__main__":
+    folder_path = sys.argv[1]
+    total = count_files(folder_path)
+    print("Total files:", total)
